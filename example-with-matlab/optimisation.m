d9c291b26d1f46a31aad6fae3038c621e879903d
MacroMelon <earwax28@gmail.com>
2026-05-31 20:42:48 +0100

    add matlab optimisation code and update test_sequence code

--- example-with-matlab/optimisation.m
+++ example-with-matlab/optimisation.m
@@ -124,63 +124,81 @@ end
 
 %-------------------------------------------------------
 
+%----- Bayesian optimisation stuff -----
+hingeAngleConstraints = [30,70];
+hingeLengthConstraints = [0,6];
+a = optimizableVariable('hinge_angle', [0,1]);
+l = optimizableVariable('hinge_length', [0,1]);
+
+%generate the latin hypercube arrays for initial points
+numStartPoints = 6;
+%initialPoints = lhsdesign(numStartPoints, 2);
+initialPoints = [0.957484147423455,0.476895926195220;0.415673824722476,0.975117665740157;0.716820546223886,0.290415290979377;0.518182791244034,0.526547124002723;0.173451429132426,0.124286303504745;0.075464078339366,0.697619195655197];
+initialPointsDeNormalised = rescale(initialPoints,[hingeAngleConstraints(1), hingeLengthConstraints(1)], [hingeAngleConstraints(2), hingeLengthConstraints(2)], "InputMin",0,"InputMax",1);
+
+function objective = responseFunction(x, angleConstraints, lengthConstraints, scaler, throttle, testLength, instrumentSerialObject, instrumentReadingRate, startTimeStripAmount, LPFFrequency)
+    deNormalisedParameters = rescale([x.hinge_angle, x.hinge_length],[angleConstraints(1), lengthConstraints(1)], [angleConstraints(2), lengthConstraints(2)], "InputMin",0,"InputMax",1);
+    input("Please attach rotor with angle: " + deNormalisedParameters(1) + " and length: " + deNormalisedParameters(2) + " and press enter... ", "s");
+    %hingeName = input("Please specify hinge file name: ", "s"); % for archival purposes
+
+    % Run Tests ----------------------
+    %before testing, get the motor spinning to ensure everything is mostly
+    %stable
+    disp("Begining test sequences");
+    disp("Starting motor...");
+    write(instrumentSerialObject, buildMotorControlPacket(0, 0, runThrottle), 'uint32');
+    pause(3);
+    
+    hingeResponse = evaluateRotor(hingeName, scaler, throttle, testLength, instrumentSerialObject, instrumentReadingRate, startTimeStripAmount, LPFFrequency);
+    %fake objective functions, for testing the framework
+    %hingeResponse = [((0.5-((x.hinge_angle-0.5)^2)) + (0.5-((x.hinge_length-0.5)^2)))*200, 0, 0];
+    %hingeResponse = [(x.hinge_angle+x.hinge_length)*200, 0, 0];
+    
+    disp("Finished Test sequences, Slowing and halting motor...");
+    haltRotor(instrumentSerialObject);
+    disp("Motor Halted!");
+    
+    objective = -hingeResponse(1);
+end
+
+% seems like on the first run, NextPoint contains an empty table
+% so only do stuff when state is 'iteration'
+function stop = onBayesoptIteration(results, state, angleConstraints, lengthConstraints)
+    disp("Current state: " + state);
+    if (strcmp(state, 'iteration'))
+        deNormalisedParameters = rescale([results.NextPoint.hinge_angle, results.NextPoint.hinge_length],[angleConstraints(1), lengthConstraints(1)], [angleConstraints(2), lengthConstraints(2)], "InputMin",0,"InputMax",1);
+        disp("Next parameters to be tested - Angle: " + deNormalisedParameters(1) + " Length: " + deNormalisedParameters(2));
+        stop = true;
+    else
+        stop = false;
+    end  
+end
+
+
 %----- Test Sequence Parameters -----
-unitTestlength = 3; % in seconds (ideally above 7 ish)
+unitTestlength = 6; % in seconds (ideally above 7 ish)
 runScaler = 300;
 runThrottle = 1000;
 
 %----- Instrumentation parameters -----
 readingRate = 1500; % in Hz (Sps)
-rampTime = 0.6; % in seconds (ideally 2)
+rampTime = 1.5; % in seconds (ideally 2)
 lowPassFilterFrequency = 200; % in Hz
 port = "/dev/ttyACM0";
 %although baudrate doesnt matter, specify anyways
 %wait... it has to be 9600 to work? straaaange....
 measurementInstrumentation = serialport(port, 9600);
 
-%for archival purposes
-hingeName = input("Please specify hinge file name: ", "s");
+% ----------- Start bayesopt -----------
 
-% Run Tests ----------------------
+bayesoptResponseFunction = @(x)responseFunction(x, hingeAngleConstraints, hingeLengthConstraints, runScaler, runThrottle, unitTestlength, measurementInstrumentation, readingRate, rampTime, lowPassFilterFrequency);
+bayesoptOutputFunction = @(results, state)onBayesoptIteration(results, state, hingeAngleConstraints, hingeLengthConstraints);
 
-%before testing, get the motor spinning to ensure everything is mostly
-%stable
-disp("Starting motor...");
-write(measurementInstrumentation, buildMotorControlPacket(0, 0, runThrottle), 'uint32');
-pause(2);
+% initial run
+% maybe look into 'InitialObjective' argument
+bayesopt(bayesoptResponseFunction, [a, l], 'MaxObjectiveEvaluations', 20, 'OutputFcn',{@saveToFile bayesoptOutputFunction}, 'PlotFcn',{@plotAcquisitionFunction, @plotObjectiveModel}, 'SaveFileName','BayesoptResults.mat', 'InitialX',array2table(initialPoints));
+% once saved file exists
+%bayesResults = load("BayesoptResults.mat", "-mat").BayesoptResults;
+%resume(bayesResults);
 
-disp("Begining test sequences");
-
-%data = performUnitTestSegment(0,0,1000,unitTestlength, measurementInstrumentation, readingRate);
-%meep = runAndProcessUnitTestSegment(270,200,800, unitTestlength, measurementInstrumentation, readingRate, "meep", rampTime, lowPassFilterFrequency);
-
-% scaler testing -------
-% !!! WARNING - DO NOT GO ABOVE 400 FOR PROLOGNED PERIODS OF TIME - RISK OF SEVERE MOTOR OVERHEATING !!!
-scalerValuesToTest = 0:10:500;
-%scalerValuesToTest = [200, 200, 200, 250, 250, 250, 300, 300, 300];
-scalerValuesToTest = flip(scalerValuesToTest);  % Remember, test high scaler values first for motor overheating reasons
-rotorResponseValues = cell2mat(cellfun(@(scalerValue) evaluateRotor(hingeName, scalerValue, runThrottle, unitTestlength, measurementInstrumentation, readingRate, rampTime, lowPassFilterFrequency), num2cell(scalerValuesToTest), 'UniformOutput', false));
-
-% throttle testing -------
-%throttleValuesToTest = 600:50:1800;
-%throttleValuesToTest = flip(throttleValuesToTest);   % test high throttle values first for motor overheating reasons
-%rotorResponseValues = cell2mat(cellfun(@(throttleValue) evaluateRotor(hingeName, 0, throttleValue, unitTestlength, measurementInstrumentation, readingRate, rampTime, lowPassFilterFrequency), num2cell(throttleValuesToTest), 'UniformOutput', false));
-
-disp("Finished Test sequences, Slowing and halting motor...");
-haltRotor(measurementInstrumentation);
-disp("Motor Halted!");
 disp("Done! :D");
-
-plot(scalerValuesToTest, rotorResponseValues(1,:), '.r', 'MarkerSize',20);
-figure;
-plot(scalerValuesToTest, rotorResponseValues(3,:), '.r', 'MarkerSize',20);
-figure;
-plot(rotorResponseValues(3,:), rotorResponseValues(1,:), '.r', 'MarkerSize',20);
-figure;
-plot(scalerValuesToTest, rotorResponseValues(2,:), '.r', 'MarkerSize',20);
-
-%plot(throttleValuesToTest, rotorResponseValues(2,:), '.r', 'MarkerSize',20);
-%figure;
-%plot(throttleValuesToTest, rotorResponseValues(3,:), '.r', 'MarkerSize',20);
-%figure;
-%plot(rotorResponseValues(2,:), rotorResponseValues(3,:), '.r', 'MarkerSize',20);
